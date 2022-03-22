8a5f66fe22575aad819c1d1eea50ee00c4ba9df3
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-03-22 14:59:44 +0200

    server setup

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express()
+const mongoose = require("mongoose");
+require("dotenv").config();
+//db connection
+//json use
+app.use(express.json)
+
+const PORT = 8000
+
+app.listen(8000,()=> console.log(`listening on port:${PORT}`) )
+//routing
